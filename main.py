be3f5d1100395ceb3db4d39f36431dd31884329b
Nobita <116506136+Nobita7618@users.noreply.github.com>
2023-10-22 19:41:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from pyrogram.errors import FloodWait
 
 load_dotenv()
 
-app = Client(name="st_userbot",
+app = Client(name="UserBot",
              api_id=int(os.getenv("API_ID")),
              api_hash=os.getenv("API_HASH"),
              session_string=os.getenv("SESSION_STRING"))
 
-bot = Client(name="st_bot",
+bot = Client(name="Bot",
              api_id=int(os.getenv("API_ID")),
              api_hash=os.getenv("API_HASH"),
              bot_token=os.getenv("BOT_TOKEN"))
@@ -35,7 +35,7 @@ async def main():
     print("Status Checker Bot Started")
     async with app:
         while True:
-            TEXT = "This is the live bot status of all Bots 🤖"
+            TEXT = "✨ **ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ғᴀʟʟᴇɴ ᴀssᴏᴄɪᴀᴛɪᴏɴ ʙᴏᴛ's sᴛᴀᴛᴜs ᴄʜᴀɴɴᴇʟ**\n\n❄ ʜᴇʀᴇ ɪs ᴛʜᴇ ʟɪsᴛ ᴏғ ᴛʜᴇ ʙᴏᴛ's ᴡʜɪᴄʜ ᴡᴇ ᴏᴡɴ ᴀɴᴅ ᴛʜᴇɪʀ sᴛᴀᴛᴜs (ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ), ᴛʜɪs ᴍᴇssᴀɢᴇ ᴡɪʟʟ ᴋᴇᴇᴘ ᴜᴘᴅᴀᴛɪɴɢ ᴏɴ **ᴇᴠᴇʀʏ 10-15 ᴍɪɴᴜᴛᴇs.**"
             for bots in BOT_LIST:
                 ok = await app.get_users(f"@{bots}")
                 try:
@@ -45,18 +45,18 @@ async def main():
                     async for x in messages:
                         msg = x.text
                     if msg == "/statusbot":
-                        TEXT += f"\n\n🤖 - **[{ok.first_name}](tg://openmessage?user_id={ok.id}): ❌ Offline**"
+                        TEXT += f"\n\n🤖 - **[{ok.first_name}](tg://openmessage?user_id={ok.id}): ❌ ᴏғғʟɪɴᴇ**"
                         if OWNER_ID:
-                            await bot.send_message(OWNER_ID, f'Alert {ok.first_name} is offline 💀')
+                            await bot.send_message(OWNER_ID, f'Alert {ok.first_name} is ᴏғғʟɪɴᴇ 💀')
                     else:
-                        TEXT += f"\n\n🤖 - **[{ok.first_name}](tg://openmessage?user_id={ok.id}): ✅ Online**\n ┗ **{msg}**"
+                        TEXT += f"\n\n🤖 - **[{ok.first_name}](tg://openmessage?user_id={ok.id}): ✅ ᴏɴʟɪɴᴇ**\n**{msg}**"
                     await app.read_chat_history(bots)
                 except FloodWait as e:
                     await asyncio.sleep(e.value)
             time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
             date = time.strftime("%d %b %Y")
             time = time.strftime("%I:%M: %p")
-            TEXT += f"\n\n--Last checked on--: \n{date}\n{time} ({TIME_ZONE})\n\n**Refreshes Automatically After Every 15 Min.**"
+            TEXT += f"\n\nʟᴀsᴛ ᴄʜᴇᴄᴋᴇᴅ ᴏɴ : \nᴅᴀᴛᴇ : {date}\nᴛɪᴍᴇ : {time}"
             await bot.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID,
                                         TEXT)
             await asyncio.sleep(900)
